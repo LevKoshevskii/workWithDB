ccdef96a06ad47a3278b68c32235c1dfaaec6da0
LevKoshevskii <koshevskii@yandex.ru>
2021-11-03 10:17:28 +0300

    commit1

--- src/test/kotlin/com/example/workWithDB/repositories/PersonRepositoryImplTest.kt
+++ src/test/kotlin/com/example/workWithDB/repositories/PersonRepositoryImplTest.kt
@@ -52,36 +52,51 @@ class PersonRepositoryImplTest {
     @Test
     fun findById() {
         val person: Person? = personRepository.findById(3)
-
-        assertThat(person?.id).isEqualTo(3)
+        assertThat(person).isEqualTo(Person(3,"Лев","Толстой"))
     }
 
     @Test
     fun addNewPerson(){
         val newPerson: Person = Person(name = "Иван", lastName = "Иванов")
         personRepository.addNewPerson(newPerson)
+        val newPersonId: Int = personRepository.findByName("Иван")[0].id
         val newPersonName: String = personRepository.getAll()[4].name
         val newPersonLastName: String = personRepository.getAll()[4].lastName
 
         assertThat(newPersonName).isEqualTo("Иван")
         assertThat(newPersonLastName).isEqualTo("Иванов")
+
+        personRepository.deleteById(newPersonId)
     }
 
     @Test
     fun updatePerson(){
+        val newPerson: Person = Person(name = "Иван", lastName = "Иванов")
+        personRepository.addNewPerson(newPerson)
+
+        val newPersonId: Int = personRepository.findByName("Иван")[0].id
+
         val updatedPerson: Person = Person(name = "Василий", lastName = "Васильев")
-        personRepository.updatePerson(4,updatedPerson)
-        val updatedPersonName: String = personRepository.findById(4)!!.name
-        val updatedPersonLastName: String = personRepository.findById(4)!!.lastName
+        personRepository.updatePerson(newPersonId,updatedPerson)
+
+        val updatedPersonName: String = personRepository.findById(newPersonId)!!.name
+        val updatedPersonLastName: String = personRepository.findById(newPersonId)!!.lastName
 
         assertThat(updatedPersonName).isEqualTo("Василий")
         assertThat(updatedPersonLastName).isEqualTo("Васильев")
+
+        personRepository.deleteById(newPersonId)
     }
 
     @Test
     fun deleteById(){
-        personRepository.deleteById(4)
-        val deletePerson: Person? = personRepository.findById(4) ?: null
+        val newPerson: Person = Person(name = "Иван", lastName = "Иванов")
+        personRepository.addNewPerson(newPerson)
+
+        val newPersonId: Int = personRepository.findByName("Иван")[0].id
+
+        personRepository.deleteById(newPersonId)
+        val deletePerson: Person? = personRepository.findById(newPersonId) ?: null
 
         assertThat(deletePerson).isEqualTo(null)
     }
